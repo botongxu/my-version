b00a63aefa51e9b12ac9533a774c0be24f3fc434
sefelix <stephen.efelix@gmail.com>
2021-07-29 15:48:12 -0400

    committing

--- src/pages/authentication/PasswordReset.tsx
+++ src/pages/authentication/PasswordReset.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
-import type { FC } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import { Helmet } from 'react-helmet-async';
+import { useEffect } from "react";
+import type { FC } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 import {
   Box,
   Card,
@@ -9,26 +9,26 @@ import {
   Container,
   Divider,
   Link,
-  Typography
-} from '@material-ui/core';
-import AuthBanner from '../../components/authentication/AuthBanner';
-import { PasswordResetAmplify } from '../../components/authentication/password-reset';
-import Logo from '../../components/Logo';
-import useAuth from '../../hooks/useAuth';
-import gtm from '../../lib/gtm';
+  Typography,
+} from "@material-ui/core";
+import AuthBanner from "../../components/authentication/AuthBanner";
+import { PasswordResetAmplify } from "../../components/authentication/password-reset";
+import Logo from "../../components/Logo";
+import useAuth from "../../hooks/useAuth";
+import gtm from "../../lib/gtm";
 
 const platformIcons = {
-  Amplify: '/static/icons/amplify.svg',
-  Auth0: '/static/icons/auth0.svg',
-  Firebase: '/static/icons/firebase.svg',
-  JWT: '/static/icons/jwt.svg'
+  Amplify: "/static/icons/amplify.svg",
+  Auth0: "/static/icons/auth0.svg",
+  Firebase: "/static/icons/firebase.svg",
+  JWT: "/static/icons/jwt.svg",
 };
 
 const PasswordReset: FC = () => {
   const { platform } = useAuth() as any;
 
   useEffect(() => {
-    gtm.push({ event: 'page_view' });
+    gtm.push({ event: "page_view" });
   }, []);
 
   return (
@@ -38,95 +38,82 @@ const PasswordReset: FC = () => {
       </Helmet>
       <Box
         sx={{
-          backgroundColor: 'background.default',
-          display: 'flex',
-          flexDirection: 'column',
-          minHeight: '100vh'
+          backgroundColor: "background.default",
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
         }}
       >
         <AuthBanner />
-        <Container
-          maxWidth="sm"
-          sx={{ py: 10 }}
-        >
+        <Container maxWidth="sm" sx={{ py: 10 }}>
           <Box
             sx={{
-              display: 'flex',
-              justifyContent: 'center'
+              display: "flex",
+              justifyContent: "center",
             }}
           >
             <RouterLink to="/">
               <Logo
                 sx={{
                   height: 40,
-                  width: 40
+                  width: 40,
                 }}
               />
             </RouterLink>
           </Box>
           <Box
             sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              mb: 8
+              display: "flex",
+              justifyContent: "center",
+              mb: 8,
             }}
           />
           <Card>
             <CardContent
               sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                p: 4
+                display: "flex",
+                flexDirection: "column",
+                p: 4,
               }}
             >
               <Box
                 sx={{
-                  alignItems: 'center',
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  mb: 3
+                  alignItems: "center",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  mb: 3,
                 }}
               >
                 <div>
-                  <Typography
-                    color="textPrimary"
-                    gutterBottom
-                    variant="h4"
-                  >
+                  <Typography color="textPrimary" gutterBottom variant="h4">
                     Password Reset
                   </Typography>
-                  <Typography
-                    color="textSecondary"
-                    variant="body2"
-                  >
+                  <Typography color="textSecondary" variant="body2">
                     Reset your account password using your code
                   </Typography>
                 </div>
                 <Box
                   sx={{
                     height: 32,
-                    '& > img': {
-                      maxHeight: '100%',
-                      width: 'auto'
-                    }
+                    "& > img": {
+                      maxHeight: "100%",
+                      width: "auto",
+                    },
                   }}
                 >
-                  <img
-                    alt="Auth platform"
-                    src={platformIcons[platform]}
-                  />
+                  <img alt="Auth platform" src={platformIcons[platform]} />
                 </Box>
               </Box>
               <Box
                 sx={{
                   flexGrow: 1,
-                  mt: 3
+                  mt: 3,
                 }}
               >
-                {platform === 'Amplify' && <PasswordResetAmplify />}
+                {platform === "Amplify" && <PasswordResetAmplify />}
               </Box>
               <Divider sx={{ my: 3 }} />
-              {platform === 'Amplify' && (
+              {platform === "Amplify" && (
                 <Link
                   color="textSecondary"
                   component={RouterLink}
