580de22c0c697db4280c0ef619f3081a2ea02d99
sefelix <stephen.efelix@gmail.com>
2021-07-29 08:44:29 -0400

    cleaned up routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -17,44 +17,6 @@ const Loadable = (Component) => (props) =>
     </Suspense>
   );
 
-// Browse pages
-
-const Browse = Loadable(lazy(() => import("./pages/browse/Browse")));
-const BrowseButtons = Loadable(
-  lazy(() => import("./pages/browse/BrowseButtons"))
-);
-const BrowseCharts = Loadable(
-  lazy(() => import("./pages/browse/BrowseCharts"))
-);
-const BrowseColors = Loadable(
-  lazy(() => import("./pages/browse/BrowseColors"))
-);
-const BrowseDetailLists = Loadable(
-  lazy(() => import("./pages/browse/BrowseDetailLists"))
-);
-const BrowseForms = Loadable(lazy(() => import("./pages/browse/BrowseForms")));
-const BrowseGridLists = Loadable(
-  lazy(() => import("./pages/browse/BrowseGridLists"))
-);
-const BrowseGroupedLists = Loadable(
-  lazy(() => import("./pages/browse/BrowseGroupedLists"))
-);
-const BrowseInputs = Loadable(
-  lazy(() => import("./pages/browse/BrowseInputs"))
-);
-const BrowseModals = Loadable(
-  lazy(() => import("./pages/browse/BrowseModals"))
-);
-const BrowseQuickStats = Loadable(
-  lazy(() => import("./pages/browse/BrowseQuickStats"))
-);
-const BrowseTables = Loadable(
-  lazy(() => import("./pages/browse/BrowseTables"))
-);
-const BrowseTypography = Loadable(
-  lazy(() => import("./pages/browse/BrowseTypography"))
-);
-
 // Authentication pages
 
 const Login = Loadable(lazy(() => import("./pages/authentication/Login")));
@@ -71,51 +33,11 @@ const VerifyCode = Loadable(
   lazy(() => import("./pages/authentication/VerifyCode"))
 );
 
-// Blog pages
-
-const BlogPostCreate = Loadable(
-  lazy(() => import("./pages/blog/BlogPostCreate"))
-);
-const BlogPostDetails = Loadable(
-  lazy(() => import("./pages/blog/BlogPostDetails"))
-);
-const BlogPostList = Loadable(lazy(() => import("./pages/blog/BlogPostList")));
-
-// Dashboard pages
-
-const Account = Loadable(lazy(() => import("./pages/dashboard/Account")));
-const Analytics = Loadable(lazy(() => import("./pages/dashboard/Analytics")));
-const Calendar = Loadable(lazy(() => import("./pages/dashboard/Calendar")));
-const Chat = Loadable(lazy(() => import("./pages/dashboard/Chat")));
-const CustomerDetails = Loadable(
-  lazy(() => import("./pages/dashboard/CustomerDetails"))
-);
-const CustomerEdit = Loadable(
-  lazy(() => import("./pages/dashboard/CustomerEdit"))
-);
 const CustomerList = Loadable(
   lazy(() => import("./pages/dashboard/CustomerList"))
 );
-const Finance = Loadable(lazy(() => import("./pages/dashboard/Finance")));
-const InvoiceDetails = Loadable(
-  lazy(() => import("./pages/dashboard/InvoiceDetails"))
-);
-const InvoiceList = Loadable(
-  lazy(() => import("./pages/dashboard/InvoiceList"))
-);
-const Kanban = Loadable(lazy(() => import("./pages/dashboard/Kanban")));
-const Mail = Loadable(lazy(() => import("./pages/dashboard/Mail")));
-const OrderDetails = Loadable(
-  lazy(() => import("./pages/dashboard/OrderDetails"))
-);
-const OrderList = Loadable(lazy(() => import("./pages/dashboard/OrderList")));
+
 const Overview = Loadable(lazy(() => import("./pages/dashboard/Overview")));
-const ProductCreate = Loadable(
-  lazy(() => import("./pages/dashboard/ProductCreate"))
-);
-const ProductList = Loadable(
-  lazy(() => import("./pages/dashboard/ProductList"))
-);
 
 // Docs pages
 
@@ -128,32 +50,6 @@ const AuthorizationRequired = Loadable(
 );
 const NotFound = Loadable(lazy(() => import("./pages/NotFound")));
 const ServerError = Loadable(lazy(() => import("./pages/ServerError")));
-
-// Projects pages
-
-const ProjectBrowse = Loadable(
-  lazy(() => import("./pages/dashboard/ProjectBrowse"))
-);
-const ProjectCreate = Loadable(
-  lazy(() => import("./pages/dashboard/ProjectCreate"))
-);
-const ProjectDetails = Loadable(
-  lazy(() => import("./pages/dashboard/ProjectDetails"))
-);
-
-// Social pages
-
-const SocialFeed = Loadable(lazy(() => import("./pages/dashboard/SocialFeed")));
-const SocialProfile = Loadable(
-  lazy(() => import("./pages/dashboard/SocialProfile"))
-);
-
-// Other pages
-
-const Checkout = Loadable(lazy(() => import("./pages/Checkout")));
-const Contact = Loadable(lazy(() => import("./pages/Contact")));
-const Home = Loadable(lazy(() => import("./pages/Home")));
-const Pricing = Loadable(lazy(() => import("./pages/Pricing")));
 
 const routes: PartialRouteObject[] = [
   {
@@ -256,6 +152,28 @@ const routes: PartialRouteObject[] = [
       {
         path: "*",
         element: <Docs />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "401",
+        element: <AuthorizationRequired />,
+      },
+      {
+        path: "404",
+        element: <NotFound />,
+      },
+      {
+        path: "500",
+        element: <ServerError />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
       },
     ],
   },
